e0a9076de415ecb67965825a63412cc6f56c8084
jairo munoz <jairo.imm@gmail.com>
2024-08-27 03:22:39 -0400

    se crea modelo flan y se agregan 8 flanes

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from web.models import flan
 
 # Register your models here.
+class FlanAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(flan, FlanAdmin)
--- web/models.py
+++ web/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class flan(models.Model):
+    flan_uuid = models.UUIDField(
+        default=uuid.uuid4,
+        editable=False,
+        unique=True
+    )
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    image_url = models.URLField()
+    slug = models.SlugField()
+    is_private = models.BooleanField(default=False)
+    
+    def __str__(self):
+        name = self.name
+        es_privado = self.is_private
+        return f'Nombre: {name} - ¿es privado?: {es_privado}'

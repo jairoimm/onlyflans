8de307e8b4f278b0ffdd9671fc903e8a9a79db15
jairo munoz <jairo.imm@gmail.com>
2024-08-27 04:01:54 -0400

    se presentan las tarjetas en el index

--- web/views.py
+++ web/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
+from web.models import flan
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    flanes = flan.objects.all()
+    flanes_privados = flan.objects.filter(is_private=True)
+    flanes_publicos = flan.objects.filter(is_private=False)
+    context = {
+        'flanes': flanes,
+        'flanes_privados': flanes_privados,
+        'flanes_publicos': flanes_publicos
+    }
+    return render(request, 'index.html', context)
 
 
 def about(request):
     return render(request, 'about.html')
 
-def welcome(request):
+def welcome(request):   
     return render(request, 'welcome.html')

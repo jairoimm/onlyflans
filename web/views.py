dea909a29d6833ae1b71a107f0168927d3213134
jairo munoz <jairo.imm@gmail.com>
2024-08-30 04:39:32 -0400

    se agregan funciones de autenticacion (login y logout)

--- web/views.py
+++ web/views.py
@@ -25,18 +25,18 @@ def welcome(request):
 
 def contacto(request):
     if request.method == 'GET':
-        form = ContactForm()                    # Se crea una instancia del formulario ContactForm sin datos iniciales.
-        context = {'form': form}                # Se crea un contexto que contiene el formulario vacío.
-        return render(request, 'contact.html', context) # Se renderiza la plantilla 'contact.html' con el contexto.
+        form = ContactForm()                               # Se crea una instancia del formulario ContactForm sin datos iniciales.
+        context = {'form': form}                           # Se crea un contexto que contiene el formulario vacío.
+        return render(request, 'contact.html', context)    # Se renderiza la plantilla 'contact.html' con el contexto.
     else:
-        form = ContactForm(request.POST)        # Se crea una instancia de ContactForm con los datos enviados en la solicitud POST.
-        if form.is_valid():                     # Se verifica si los datos del formulario son válidos.
+        form = ContactForm(request.POST)                   # Se crea una instancia de ContactForm con los datos enviados en la solicitud POST.
+        if form.is_valid():                                # Se verifica si los datos del formulario son válidos.
             contact.objects.create(
                 **form.cleaned_data
-            )                                   # Esta es la forma de pedirle a un modelo que cree un registro usando los datos de un formulario
-            return redirect('/exito')         # Si el formulario es válido, se redirige al usuario a la URL '/success'.
-        context = {'form': form}                # Se crea un contexto que contiene el formulario con los datos (válidos o no).
-        return render(request, 'contact.html', context) # Se vuelve a renderizar la plantilla con el contexto actualizado.        
+            )                                              # Esta es la forma de pedirle a un modelo que cree un registro usando los datos de un formulario
+            return redirect('/exito')                      # Si el formulario es válido, se redirige al usuario a la URL '/success'.
+        context = {'form': form}                           # Se crea un contexto que contiene el formulario con los datos (válidos o no).
+        return render(request, 'contact.html', context)    # Se vuelve a renderizar la plantilla con el contexto actualizado.        
 
 def success(request):
     return render(request, 'success.html')
